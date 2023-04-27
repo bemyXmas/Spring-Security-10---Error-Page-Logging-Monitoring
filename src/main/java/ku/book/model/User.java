f7bcd200e39c83fbd91965a3efaf32fd137a2690
SirapopKunjiak <sirapop.ku@ku.th>
2023-04-27 17:09:51 +0700

    finish

---
+++ src/main/java/ku/book/model/User.java
@@ -0,0 +1,30 @@
+package ku.book.model;
+
+import ku.book.config.AttributeEncryptor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Convert;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.Instant;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Convert(converter = AttributeEncryptor.class)
+    private String username;
+    private String password;
+    @Convert(converter = AttributeEncryptor.class)
+    private String name;
+
+    private Instant createdAt;
+}
